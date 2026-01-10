38b059663f1cfadcf7abe14ab722d488d20cb81a
khushal1513 <khushal122005@gmail.com>
2026-01-10 14:53:08 +0530

    full chnges

--- AI/main.py
+++ AI/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=["http://localhost:3000",
                    "http://localhost:5173",
                    "http://localhost:5174",
+                   "https://acadmate-lac.vercel.app"
                    ],
     allow_credentials=True,
     allow_methods=["*"],
